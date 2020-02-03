be84453845b838b3d8020f25ab7b0c907c95795e
ColdAsMilk <32677787+ColdAsMilk@users.noreply.github.com>
2020-02-03 08:34:06 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecipeeWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
